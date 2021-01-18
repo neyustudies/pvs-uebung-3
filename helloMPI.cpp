67cf528ce0d875b4eb444a32db4050e2a60bb9d7
Jakob Ruckel <jakob@ruckel.de>
2021-01-18 22:21:54 +0100

    Format code

--- helloMPI.cpp
+++ helloMPI.cpp
@@ -1,20 +1,21 @@
-#include<stdio.h>
-#include<mpi.h>	// include MPI header file
- 
+#include <mpi.h>  // include MPI header file
+#include <stdio.h>
+
 int main(int argc, char** argv) {
-	int nodeID, numNodes;
+    int nodeID, numNodes;
 
-	/* MPI environment */
-	MPI_Init(&argc, &argv);										// initializes MPI library, first MPI function call
-	MPI_Comm_size(MPI_COMM_WORLD, &numNodes);	// returns the total number of processes
-	MPI_Comm_rank(MPI_COMM_WORLD, &nodeID);		// returns the rank (ID) of the calling MPI process
+    /* MPI environment */
+    MPI_Init(&argc, &argv);  // initializes MPI library, first MPI function call
+    MPI_Comm_size(MPI_COMM_WORLD,
+                  &numNodes);  // returns the total number of processes
+    MPI_Comm_rank(MPI_COMM_WORLD,
+                  &nodeID);  // returns the rank (ID) of the calling MPI process
 
-	/* writes formatted output to stdout */
-	printf("Hello world from process %d of %d\n", nodeID, numNodes);
+    /* writes formatted output to stdout */
+    printf("Hello world from process %d of %d\n", nodeID, numNodes);
 
-	/* terminates MPI environment, last MPI function call */
-	MPI_Finalize();
+    /* terminates MPI environment, last MPI function call */
+    MPI_Finalize();
 
-	return 0;
-
+    return 0;
 }
