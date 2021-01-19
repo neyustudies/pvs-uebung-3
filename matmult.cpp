da27263bc7e6317352e6f96f1971dfc69354c382
Jakob Ruckel <jakob@ruckel.de>
2021-01-19 01:10:23 +0100

    add distributed multiplication, variant 1

--- matmult.cpp
+++ matmult.cpp
@@ -1,10 +1,13 @@
+#include <assert.h>
+#include <mpi.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <mpi.h>
 
-#define MAT_SIZE 100
+#define MAT_SIZE 1000
 
 #define MASTER_ID 0
+
+#define EPSILON 0.0001f
 
 // ---------------------------------------------------------------------------
 // allocate space for empty matrix A[row][col]
@@ -53,7 +56,31 @@ void free_mat(float** A, int num_rows) {
 }
 
 // ---------------------------------------------------------------------------
+// Check two matrices for equality
+bool mat_equal(float** mat1, float** mat2, int m, int n) {
+    for (int i = 0; i < m; ++i) {
+        for (int j = 0; j < n; ++j) {
+            if (abs(mat1[i][j] - mat2[i][j]) > EPSILON) {
+                return false;
+            }
+        }
+    }
+    return true;
+}
 
+// ---------------------------------------------------------------------------
+// Number of rows of A that will be transferred to the worker.
+// Will not always be even, e.g. 333, 333 and 334 for 1000x1000
+// matrices and 3 workers.
+int calc_num_rows_part(int worker_id, int num_workers) {
+    int num_rows_part = MAT_SIZE / num_workers;
+    if (worker_id == num_workers) {
+        num_rows_part += MAT_SIZE % num_workers;
+    }
+    return num_rows_part;
+}
+
+// ---------------------------------------------------------------------------
 int main(int argc, char* argv[]) {
     int node_id, num_nodes, num_workers;
 
@@ -80,7 +107,7 @@ int main(int argc, char* argv[]) {
         init_mat(A, d1, d2);
         B = alloc_mat(d2, d3);
         init_mat(B, d2, d3);
-        printf("Master:  B[4][2] is:  %.f\n", B[4][2]);
+
         // no initialisation of C, because it gets filled by matmult
         C_ser = alloc_mat(d1, d3);
         C_par = alloc_mat(d1, d3);
@@ -92,45 +119,50 @@ int main(int argc, char* argv[]) {
             for (j = 0; j < d3; j++)
                 for (k = 0; k < d2; k++)
                     C_ser[i][j] += A[i][k] * B[k][j];
-        printf("Done in %.3f seconds!\n", MPI_Wtime() - start);
+        printf("[Serial]  Done in %.3f seconds!\n", MPI_Wtime() - start);
 
+        printf("Perform parallel matrix multiplication...\n");
         // Send B and relevant parts of A to workers.
+        double start_par = MPI_Wtime();
         for (int worker_id = 1; worker_id < num_nodes; ++worker_id) {
             // Send B.
             MPI_Send(*B, MAT_SIZE * MAT_SIZE, MPI_FLOAT, worker_id, 0,
                      MPI_COMM_WORLD);
 
-            // Number of rows of A that will be transferred to the worker.
-            // Will not always be even, e.g. 333, 333 and 334 for 1000x1000
-            // matrices and 3 workers.
-            int n_rows_a = MAT_SIZE / num_workers;
-            if (worker_id == num_workers) {
-                n_rows_a += MAT_SIZE % num_workers;
-            }
-            MPI_Send(&n_rows_a, 1, MPI_FLOAT, worker_id, 0, MPI_COMM_WORLD);
+            int num_rows_part = calc_num_rows_part(worker_id, num_workers);
+            MPI_Send(&num_rows_part, 1, MPI_FLOAT, worker_id, 0,
+                     MPI_COMM_WORLD);
 
-            float** part_A = alloc_mat(n_rows_a, MAT_SIZE);
+            float** part_A = alloc_mat(num_rows_part, MAT_SIZE);
             int row_offset = (worker_id - 1) * (MAT_SIZE / num_workers);
-            for (int row = 0; row < n_rows_a; ++row) {
+            for (int row = 0; row < num_rows_part; ++row) {
                 for (int col = 0; col < MAT_SIZE; ++col) {
                     part_A[row][col] = A[row + row_offset][col];
                 }
             }
             // Send part_A.
-            MPI_Send(*part_A, n_rows_a * MAT_SIZE, MPI_FLOAT, worker_id, 0,
+            MPI_Send(*part_A, num_rows_part * MAT_SIZE, MPI_FLOAT, worker_id, 0,
                      MPI_COMM_WORLD);
-
-            // float* buf_A = (float*)calloc(n_rows_a, sizeof(float));
-            //*buf = 42;
-            // MPI_Send(buf, 1, MPI_FLOAT, worker_id, 0, MPI_COMM_WORLD);
         }
+        printf("[Parallel]  Done sending data in %.3f seconds!\n",
+               MPI_Wtime() - start_par);
 
-        /* test output */
-        // print_mat(A, d1, d2, "A");
-        // print_mat(B, d2, d3, "B");
-        // print_mat(C, d1, d3, "C");
+        // Collect data into C.
+        double start_par_collect = MPI_Wtime();
+        for (int worker_id = 1; worker_id < num_nodes; ++worker_id) {
+            int row_offset = (worker_id - 1) * (MAT_SIZE / num_workers);
+            int num_rows_part = calc_num_rows_part(worker_id, num_workers);
+            MPI_Recv(*C_par + (MAT_SIZE * row_offset), num_rows_part * MAT_SIZE,
+                     MPI_FLOAT, worker_id, 0, MPI_COMM_WORLD, &status);
+        }
+        printf("[Parallel]  Done collecting data in %.3f seconds!\n",
+               MPI_Wtime() - start_par_collect);
+        printf("[Parallel]  Done in a total of %.3f seconds!\n",
+               MPI_Wtime() - start_par);
 
-        printf("\nDone.\n");
+        assert(mat_equal(C_ser, C_par, MAT_SIZE, MAT_SIZE));
+
+        printf("\nDone :)\n");
 
         /* free dynamic memory */
         free_mat(A, d1);
@@ -139,35 +171,46 @@ int main(int argc, char* argv[]) {
         free_mat(C_par, d1);
 
     } else {
-        printf("Waiting for stuff to do (%d/%d)...\n", node_id, num_nodes);
-
+        double start = MPI_Wtime();
         float** B = alloc_mat(MAT_SIZE, MAT_SIZE);
         MPI_Recv(*B, MAT_SIZE * MAT_SIZE, MPI_FLOAT, MASTER_ID, 0,
                  MPI_COMM_WORLD, &status);
 
-        printf("Worker: B[4][2] is:  %.f\n", B[4][2]);
+        int num_rows_part;
+        MPI_Recv(&num_rows_part, 1, MPI_INT, MASTER_ID, 0, MPI_COMM_WORLD,
+                 &status);
 
-        int n_rows_a;
-        MPI_Recv(&n_rows_a, 1, MPI_INT, MASTER_ID, 0, MPI_COMM_WORLD, &status);
-
-        printf("Will get %d rows of A (%d/%d)...\n", n_rows_a, node_id,
-               num_nodes);
-
-        float** part_A = alloc_mat(n_rows_a, MAT_SIZE);
-        MPI_Recv(*part_A, n_rows_a * MAT_SIZE, MPI_FLOAT, MASTER_ID, 0,
+        float** part_A = alloc_mat(num_rows_part, MAT_SIZE);
+        MPI_Recv(*part_A, num_rows_part * MAT_SIZE, MPI_FLOAT, MASTER_ID, 0,
                  MPI_COMM_WORLD, &status);
 
-        float** part_C = alloc_mat(n_rows_a, MAT_SIZE);
+        printf("[Parallel#%d]  Done recieving in %.3f seconds!\n", node_id,
+               MPI_Wtime() - start);
+        double start_calc = MPI_Wtime();
 
-        for (int i = 0; i < n_rows_a; ++i) {
+        float** part_C = alloc_mat(num_rows_part, MAT_SIZE);
+
+        for (int i = 0; i < num_rows_part; ++i) {
             for (int j = 0; j < MAT_SIZE; ++j) {
                 for (int k = 0; k < MAT_SIZE; ++k) {
                     part_C[i][j] += part_A[i][k] * B[k][j];
                 }
             }
         }
+        printf("[Parallel#%d]  Done calculating in %.3f seconds!\n", node_id,
+               MPI_Wtime() - start_calc);
+        double start_send = MPI_Wtime();
 
-        // printf("Is this the answer? %.f\n", *buf);
+        MPI_Send(*part_C, num_rows_part * MAT_SIZE, MPI_FLOAT, MASTER_ID, 0,
+                 MPI_COMM_WORLD);
+
+        printf("[Parallel#%d]  Done sending in %.3f seconds!\n", node_id,
+               MPI_Wtime() - start_send);
+
+        free_mat(B, MAT_SIZE);
+        free_mat(part_A, num_rows_part);
+        free_mat(part_C, num_rows_part);
+
     }
 
     MPI_Finalize();
